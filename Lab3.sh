9edc29d3ba51aa2c8b616369d9f6052bf4348464
RohitSatheeshNair <71492948+RohitSatheeshNair@users.noreply.github.com>
2021-12-01 22:51:55 +0530

    Create Lab3.sh

---
+++ Lab3.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Enter 3 numbers "
+read n1 n2 n3
+
+if [ $n1 -gt $n2  -a  $n1 -gt $n3 ]
+then
+echo "n1 is the greatest"
+
+elif [ $n2 -gt $n1  -a  $n2 -gt $n3 ]
+then
+echo "n2 is the greatest"
+
+else 
+echo "n3 is the greatest "
+
+fi
