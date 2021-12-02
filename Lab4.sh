75ad5572fe2955765427671c6f68a0d0729e9bc4
RohitSatheeshNair <71492948+RohitSatheeshNair@users.noreply.github.com>
2021-12-02 10:30:03 +0530

    Update Lab4.sh

--- Lab4.sh
+++ Lab4.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
-echo "enter 2 numbers : "
+echo  -n "enter 2 numbers : "
 read n1 n2 
-echo " enter the choice 1)add 2) mull3)sub 4)div "
+echo " enter the choice
+		 1)add 
+                 2) mull 
+                 3)sub 
+                 4)div "
 read ch
 case $ch in
 1)echo  "$n1 + $n2" | bc ;;
 2)echo  "$n1 * $n2 " | bc ;;
 3)echo  "$n1 - $n2 " | bc ;;
-4)echo  "$n1/$n2 " | bc ;;
+4)echo  "scale=2 ; $n1 / $n2" | bc ;;
 esac
