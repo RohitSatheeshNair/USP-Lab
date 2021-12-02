134d1a38faeb96f03b88b7035d014b74e6d59f71
RohitSatheeshNair <71492948+RohitSatheeshNair@users.noreply.github.com>
2021-12-02 09:53:49 +0530

    Update lab7.sh

--- lab7.sh
+++ lab7.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Enter basic salary "
 read basic
-DA=` expr $basic/10 `
-HRA=` expr $basic/5 `
+DA=` expr $basic/0.1 `
+HRA=` expr $basic/0.2 `
 
 gross=$(echo "$basic + $DA + $HRA" | bc)
 echo "gross salary : $gross "
